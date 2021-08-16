744a3904991407a3a9b2039e584fbcfa8760f1ae
Majed Al-Jefri <38035120+Maj27@users.noreply.github.com>
2021-08-16 12:06:17 -0600

    Time: 108 ms (73.04%), Space: 15.5 MB (93.14%) - LeetHub

---
+++ single-number/single-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        xor = nums[0]
+        for i in range(1,len(nums)):
+            xor = xor^nums[i]
+            
+        return xor
+             
