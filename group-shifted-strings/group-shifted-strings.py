de0eb6f2a443d9a99145ef981650bdbb244eecf2
Majed Al-Jefri <38035120+Maj27@users.noreply.github.com>
2021-09-08 07:44:17 -0600

    Time: 30 ms (25.49%), Space: 13.6 MB (53.50%) - LeetHub

---
+++ group-shifted-strings/group-shifted-strings.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def groupStrings(self, strings):
+        """
+        :type strings: List[str]
+        :rtype: List[List[str]]
+        """
+        
+        def hash_str(s):
+            return tuple((ord(c)-ord(s[0]))%26 for c in s)
+        
+        dic = collections.defaultdict(list)
+        for s in strings:
+            dic[hash_str(s)].append(s)
+            
+        return dic.values()
+        
