691aa5093be26bf68f9d799d7dc35a8081451a42
Majed Al-Jefri <38035120+Maj27@users.noreply.github.com>
2021-10-09 13:50:05 -0600

    Time: 64 ms (60.57%), Space: 16.7 MB (90.24%) - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.py
+++ meeting-rooms-ii/meeting-rooms-ii.py
@@ -4,27 +4,22 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: int
         """
+                
+        start = sorted([i[0] for i in intervals])
+        end = sorted([i[1] for i in intervals])
         
-        rooms = 0
-        if len(intervals)==1:
-            return 1
-        
-        start = sorted([x[0] for x in intervals])
-        end = sorted([x[1] for x in intervals])
-        
-        i ,j = 0,0
-        print(start)
-        print(end)
-        tmp = 0
-        while i<len(start):
-            if start[i]<end[j]:
-                i+=1
-                tmp+=1
-                rooms = max(rooms,tmp)
+        minRooms = 0
+        curRooms = 0
+        s,e = 0,0
+        while s<len(start): 
+            if start[s]<end[e]:
+                curRooms+=1
+                s+=1
             else:
-                j+=1
-                tmp-=1
+                curRooms-=1
+                e+=1
             
+            minRooms = max(minRooms,curRooms)
             
-        return rooms
-            
+        return minRooms
+                
