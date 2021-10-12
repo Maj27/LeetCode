dae9555dcae64453da6be0021463a92e9c3da120
Majed Al-Jefri <38035120+Maj27@users.noreply.github.com>
2021-10-12 14:22:58 -0600

    Time: 463 ms (29.23%), Space: 14.5 MB (79.87%) - LeetHub

---
+++ word-ladder/word-ladder.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: int
+        
+        wordList = set(wordList)
+        queue = collections.deque([[beginWord, 1]])
+        while queue:
+            word, length = queue.popleft()
+            if word == endWord:
+                return length
+            for i in range(len(word)):
+                for c in 'abcdefghijklmnopqrstuvwxyz':
+                    next_word = word[:i] + c + word[i+1:]
+                    if next_word in wordList:
+                        wordList.remove(next_word)
+                        queue.append([next_word, length + 1])
+        return 0
+        """
+    
+        wordList = set(wordList)
+        
+        length = 1
+        queue = deque([beginWord])
+        while queue:
+            
+            new_q = deque()
+            length+=1
+            while queue:
+                word = queue.popleft()
+                for i in range(len(word)):
+                    for c in 'abcdefghijklmnopqrstuvwxyz':
+                        next_word = word[:i] + c + word[i+1:]
+                        if next_word == endWord and endWord in wordList:
+                            return length
+                        if next_word in wordList:
+                            wordList.remove(next_word)
+                            new_q.append(next_word)
+            queue = new_q
+            
+        return 0
