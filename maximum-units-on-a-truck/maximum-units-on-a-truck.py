7a8f84145f3d07c24d46b217ea158f244eab1886
Majed Al-Jefri <38035120+Maj27@users.noreply.github.com>
2021-11-02 22:01:35 -0600

    Time: 124 ms (89.65%), Space: 14.1 MB (33.22%) - LeetHub

--- maximum-units-on-a-truck/maximum-units-on-a-truck.py
+++ maximum-units-on-a-truck/maximum-units-on-a-truck.py
@@ -5,16 +5,19 @@ class Solution(object):
         :type truckSize: int
         :rtype: int
         """
+        boxTypes.sort(key=lambda x:x[1],reverse= True)
 
-        boxTypes.sort(key=lambda x:x[1],reverse=True)
-        maxBoxes = 0
-        
-        for boxes, units in boxTypes:
-            if truckSize>=boxes:
-                maxBoxes += boxes*units
-                truckSize -= boxes
+        i = 0
+        ans = 0
+        while truckSize >0 and i<len(boxTypes):
+            
+            q, v = boxTypes[i]
+            if q<=truckSize:
+                ans+=q*v        
             else:
-                maxBoxes += truckSize*units
-                break
-        
-        return maxBoxes
+                ans+=truckSize*v
+            truckSize-=q
+            i+=1
+            
+        return ans
+            
