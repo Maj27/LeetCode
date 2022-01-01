7daab963eea36c02d8f6029f5e15a79bae8d3d56
Majed Al-Jefri <38035120+Maj27@users.noreply.github.com>
2022-01-01 10:52:04 -0700

    Time: 56 ms (89.37%), Space: 15.3 MB (40.56%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,14 +1,11 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         dic = {}
-        for i in range(len(nums)):
-            comp = target-nums[i]
-            if comp in dic:
-                return [dic[comp],i]
-            dic[nums[i]]=i
-                            
+        for i,x in enumerate(nums):
+            if target-x in dic:
+                return [i,dic[target-x]]
+            dic[x]=i
+            
+            
+            
+           
