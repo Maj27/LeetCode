0bc934b90dabcf824fb3eba801e8e171e78a95d7
Majed Al-Jefri <38035120+Maj27@users.noreply.github.com>
2022-01-14 14:39:26 -0700

    Time: 292 ms (32.70%), Space: 21.1 MB (59.34%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -3,21 +3,17 @@ class Solution:
 
         
         N = len(nums)
-        leftProd = [0]*N
-        rightProd = [0]*N
+        sol = [0]*N
         
         prod = 1 
         for i in range(N):
-            leftProd[i] = prod
+            sol[i] = prod
             prod = prod *  nums[i]
             
         prod = 1 
         for i in range(N-1,-1,-1):
-            rightProd[i] = prod
+            sol[i] = prod * sol[i]
             prod = prod *  nums[i]
 
-        
-        for i in range(N):
-            leftProd[i]*=rightProd[i]
-            
-        return leftProd
+                  
+        return sol
