c831e9efa4e59939a06b0c34db8de127a19981d9
Majed Al-Jefri <38035120+Maj27@users.noreply.github.com>
2022-01-30 11:23:59 -0700

    Time: 563 ms (17.70%), Space: 15.6 MB (33.48%) - LeetHub

---
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -0,0 +1,38 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(heights), len(heights[0])
+        pacific, atlantic = set(), set()
+        
+        directions = [[-1,0],[1,0],[0,-1],[0,1]]
+        
+        def dfs(r, c, visited, prevHeight):
+            if (r, c) in visited or \
+                r<0 or c<0 or r==ROWS or c==COLS or \
+                heights[r][c] < prevHeight:
+                    return
+            visited.add((r,c))
+            for i,j in directions:
+                dfs(r+i, c+j, visited, heights[r][c])
+                
+        #add first row and col to pacific
+        for c in range(COLS):# row 0
+            dfs(0, c, pacific, heights[0][c])
+        for r in range(ROWS): #col 0 
+            dfs(r, 0, pacific, heights[r][0])
+        
+        #add last row and col to atlantic
+        for c in range(COLS):# row n-1
+            dfs(ROWS-1,c, atlantic, heights[ROWS-1][c])
+        for r in range(ROWS): #col m-1 
+            dfs(r,COLS-1, atlantic, heights[r][COLS-1])
+              
+                        
+        # now check if each cell in both add to sol
+        sol = []
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r,c) in pacific and (r,c) in atlantic:
+                    sol.append([r,c])
+             
+        return sol
+        
