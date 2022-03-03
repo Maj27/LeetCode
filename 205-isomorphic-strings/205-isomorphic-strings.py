7f9178be486357367ac229060dffc0ee1a099d82
Majed Al-Jefri <38035120+Maj27@users.noreply.github.com>
2022-03-03 14:48:00 -0700

    Time: 36 ms (97.05%), Space: 14.2 MB (93.83%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -1,17 +1,16 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        mapping = {}
-        for a,b in zip(s,t):
-            if a not in mapping:
-                mapping[a] = b
-            else:
-                if mapping[a]!=b:
-                    return False
-        mapping = {}
-        for a,b in zip(t,s):
-            if a not in mapping:
-                mapping[a] = b
-            else:
-                if mapping[a]!=b:
-                    return False
-        return True
+        
+        def checkMapping(st1, st2):
+            mapping = {}
+            
+            for a,b in zip(st1,st2):
+                if a not in mapping:
+                    mapping[a] = b
+                else:
+                    if mapping[a]!=b:
+                        return False
+       
+            return True
+            
+        return checkMapping(s, t) and checkMapping(t, s)
